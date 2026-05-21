568b52698d8f81ce3ba8bbf51a321d0eca3d0bbc
gatosuco <54418139+gatosuco@users.noreply.github.com>
2026-05-21 14:25:24 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_registro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
