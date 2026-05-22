c5c886de344b4f139446a665f6f531e569964293
Danny Encalada <danny.encalada@scpm.gob.ec>
2026-05-22 14:47:38 -0500

    actualizar

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web_registro.Models;
+
+namespace web_registro.Controllers
+{
+    public class ClientesController : Controller
+    {
+        //Simular una bdd
+        private static List<ClienteModel> _lista_Clientes = new List<ClienteModel>()
+        {
+            new ClienteModel
+            {
+                id = 1,
+                nombres = "Danny ",
+                apellidos = "Encalada",
+                direccion = "Pichincha-Quito",
+                telefono = "0995725877",
+                correo = "danny60@uniandes.edu.ec"
+            },
+            new ClienteModel
+            {
+                id = 2,
+                nombres = "Carlos",
+                apellidos = "Sanchea",
+                direccion = "Quito",
+                telefono = "0984001025",
+                correo = "carlos.andr@uniandes.edu.ec"
+            }
+        };
+        // GET: ClientesController
+        public ActionResult Index()
+        {
+            return View(_lista_Clientes);
+        }
+
+        // GET: ClientesController/Details/5
+        public ActionResult Details(int id)
+        {
+            var cliente = _lista_Clientes.FirstOrDefault(c => c.id == id);
+            if (cliente == null) return NotFound();
+            return View(cliente);
+        }
+
+        // GET: ClientesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClienteModel cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                cliente.id = _lista_Clientes.Count > 0 ? _lista_Clientes.Max(c => c.id) + 1 : 1;
+
+                /*if (_lista_Clientes.Count > 0)
+                {
+                    cliente.id = _lista_Clientes.Count() + 1;
+                }
+                else
+                {
+                    cliente.id = 1;
+                }*/
+                _lista_Clientes.Add(cliente);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(cliente); 
+            }
+        }
+
+        // GET: ClientesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var cliente = _lista_Clientes.FirstOrDefault(c => c.id == id);
+            if (cliente == null) return NotFound();
+            return View(cliente);
+        }
+
+        // POST: ClientesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, ClienteModel cliente)
+        {
+            if (id != cliente.id) return BadRequest("No se encontro el cliente");
+            
+            
+                var clienteExistente = _lista_Clientes.FirstOrDefault(c => c.id == id);
+
+            if (clienteExistente == null)
+            {
+                return NotFound();
+            }
+
+
+            clienteExistente.id = cliente.id;
+                clienteExistente.nombres = cliente.nombres;
+                clienteExistente.apellidos = cliente.apellidos;
+                clienteExistente.direccion = cliente.direccion;
+                clienteExistente.telefono = cliente.telefono;
+                clienteExistente.correo = cliente.correo;
+
+                return RedirectToAction(nameof(Index));
+            
+            
+        }
+
+        // GET: ClientesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var cliente = _lista_Clientes.FirstOrDefault(c => c.id == id);
+            if (cliente == null) return NotFound();
+            return View(cliente);
+        }
+
+        // POST: ClientesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            var cliente = _lista_Clientes.FirstOrDefault(c => c.id == id);
+            if (cliente == null) return NotFound();
+            _lista_Clientes.Remove(cliente);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/ClienteModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_registro.Models
+{
+    public class ClienteModel
+    {
+        // [Key] define la clave primaria si no se usa <id> por defecto
+        public int id { get; set; }
+
+        [Display(Name = "Nombres de Clientes")]
+        [MaxLength(100, ErrorMessage = "El número máximo de carácteres es 100")]
+        [Required(ErrorMessage = "El campo es requerido")]
+        public string nombres { get; set; } = "";
+
+        [MaxLength(100, ErrorMessage = "El número máximo de carácteres es 100")]
+        [Required(ErrorMessage = "El campo es requerido")]
+        public string apellidos { get; set; } = "";
+
+        [Required(ErrorMessage = "El campo dirección es requerido")]
+        public string direccion { get; set; } = "";
+
+        [MaxLength(17, ErrorMessage = "El número máximo de carácteres es 17")]
+        [MinLength(9, ErrorMessage = "El mínimo de carácteres es 9")]
+        [Required(ErrorMessage = "El campo es requerido")]
+        public string telefono { get; set; } = "";
+
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+        public string correo { get; set; } = "";
+    }
+}
